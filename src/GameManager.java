50e8f0dbf1623a6bb9ac887bfb1a07b9ae691db9
Gary Pollock <gdpollock@gmail.com>
2022-07-14 08:10:55 -0500

    Completed Tutorial 3

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,6 +1,11 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
 
 public class GameManager {
 	// Dimension for gameplay levels
 	public final static int SCREEN_WIDTH = 1024;
 	public final static int SCREEN_HEIGHT = 768;
+	
+	public static AudioInputStream astream;
+	public static Clip aclip;
 }
--- src/Intro.java
+++ src/Intro.java
@@ -7,15 +7,45 @@ public class Intro extends JPanel implements Runnable{
 	private JFrame window = new JFrame();
 	private String imagePath = "images\\intro.jpg";
 	private int wait = 2000;	//2 seconds
+	private String audioFileName = "audio\\bg-music.wav";
 	
 	public Intro(){	//default constructor
+		//Play audio clip as the scene starts
+		playAudio();
+
+		//Set screen size
 		this.setPreferredSize(new Dimension(GameManager.SCREEN_WIDTH,
 				GameManager.SCREEN_HEIGHT));
+		
+		//Add this frame to the window and set it up
 		window.add(this);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.pack();
-		//window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//window.setExtendedState(JFrame.MAXIMIZED_BOTH);	//only if you want fullscreen
+		
+		//Open the scene
 		window.setVisible(true);
+	}
+	
+	public void playAudio() {
+		try {
+			File audioPath = new File(audioFileName);
+			//We need java's audio input stream
+			GameManager.astream = AudioSystem.getAudioInputStream(audioPath);
+
+			//Reference audio clip class
+			GameManager.aclip = AudioSystem.getClip();
+
+			//Open the stream using the clip class
+			GameManager.aclip.open(GameManager.astream);
+
+			//Start the music
+			GameManager.aclip.start();
+		
+		} //end of the try block
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void paint(Graphics g) {
